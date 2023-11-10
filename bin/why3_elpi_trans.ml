3603a8b6f89b5ccb3282c2d69795616e1b305629
Matteo Manighetti <matteo.manighetti@inria.fr>
2023-11-10 18:21:04 +0100

    Stub for quotations

--- bin/why3_elpi_trans.ml
+++ bin/why3_elpi_trans.ml
@@ -23,7 +23,7 @@ let query (arg: string) (e: Env.env) (t : Task.task) =
   let builtins = [Elpi.API.BuiltIn.declare ~file_name:"builtins.elpi" (transform_builtins @ Elpi.Builtin.std_declarations)] in
   let elpi = (API.Setup.init ~builtins ~file_resolver:(Elpi.API.Parse.std_resolver ~paths:[] ()) ()) in
   let loc = Elpi.API.Ast.Loc.initial "(elpi)" in
-  let ast = Elpi.API.Parse.program ~elpi ~files:["test.elpi"] in
+  let ast = Elpi.API.Parse.program ~elpi ~files:["transform.elpi"] in
   let prog =
     let flags = Elpi.API.Compile.default_flags in
     ast |>
@@ -33,20 +33,38 @@ let query (arg: string) (e: Env.env) (t : Task.task) =
   if not (Debug.test_flag debug_no_typecheck) && not (Elpi.API.Compile.static_check ~checker:(Elpi.Builtin.default_checker ()) main_query)
     then Loc.errorm "elpi: type error in file"
   else
-  let out_decl_list =
+  let out_task =
   match Elpi.API.Execute.once (Elpi.API.Compile.optimize main_query) with
   | Elpi.API.Execute.Success { output = (tm, ()); _ } ->
     Format.printf "elpi: success\n%!" ; tm
   | Failure -> Loc.errorm "elpi: failure"
   | NoMoreSteps -> assert false
   in
-  out_decl_list
+  out_task
+
+exception Arg_parse_type_error of Loc.position * string * exn
+
+let build_quotation (naming_table: Trans.naming_table)  : Elpi.API.Quotation.quotation = 
+  fun ~depth st _loc text ->
+   let ns = naming_table.namespace in
+   let km = naming_table.known_map in
+   let c = naming_table.coercion in
+   (* let open Parsing in *)
+   let tm = 
+     try
+       let lb = Lexing.from_string text in
+       let t = Lexer.parse_term lb in
+       Typing.type_term_in_namespace ns km c t
+     with Loc.Located (loc, e) -> raise (Arg_parse_type_error (loc, text, e))
+   in let st, t, _ = term.embed ~depth st tm
+   in st, t
 
 let elpi_trans : Trans.trans_with_args = 
-  fun argl env _namtab _name  ->
-    match argl with
-    | [arg] -> (Trans.store (query arg env))
-    | _ -> Loc.errorm "elpi: wrong number of arguments"
+  fun argl env naming_table _name  ->
+  let () = API.Quotation.set_default_quotation (build_quotation naming_table) in
+  match argl with
+  | [arg] -> (Trans.store (query arg env))
+  | _ -> Loc.errorm "elpi: wrong number of arguments"
 
 (* let () = Trans.register_transform "elpi_query" elpi_trans
 ~desc:"Run@ a@ simple@ elpi@ command" *)
