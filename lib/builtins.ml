29c4c7709b76cc94622d091672a398a16a36351f
Matteo Manighetti <matteo.manighetti@inria.fr>
2024-06-07 18:31:15 +0200

    Some cleanup, enable quotations and builtins

---
+++ lib/builtins.ml
@@ -0,0 +1,31 @@
+open Why3_elpi
+let why3_builtin_declarations =
+  let open Elpi.API.BuiltIn in
+  let open Elpi.API.BuiltInData in
+  let open Elpi.API.BuiltInPredicate in
+  let open Elpi.API.BuiltInPredicate.Notation in
+  [MLCode
+      ( CPred ( "why3.var-type", in_ctx_for_ty,
+            CIn  (vsymbol, "V",
+            COut (ty, "T",
+            CEasy "Get the type of a variable" )),
+            fun var _ ~depth:_ _ctx  _-> !: (var.vs_ty)),
+        DocAbove );
+  MLCode
+  ( CPred ( "why3.ls-type", in_ctx_for_ty,
+            CIn  (lsymbol, "L",
+            COut (ty, "T",
+            CEasy "Get the value type of a logic symbol. Fails if the symbol has no value type (i.e. is a proposition)" )),
+            fun ls _ ~depth:_ _ctx  _-> ?: (ls.ls_value)),
+        DocAbove );
+  MLCode
+  ( CPred ("why3.pp-term", in_ctx_for_term,
+            CIn  (term, "T",
+            COut (Elpi.API.BuiltInContextualData.string, "S",
+            CEasy "Convert a term to string using Why3's pretty printer" )),
+            fun t _ ~depth:_ ctx  _-> !: 
+             (Format.asprintf "@[<hov>%a@ |-@ %a@]@\n%!"
+      (Elpi.API.RawData.Constants.Map.pp (Elpi.API.ContextualConversion.pp_ctx_entry pp_ctx_for_term)) ctx#ctx_for_term
+       term.pp t)),
+        DocAbove );
+  ]
